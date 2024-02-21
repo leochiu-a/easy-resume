17ac0150b29687de2c74263e6d93b1cc83559cdb
leochiu <leochiu@hahow.in>
2024-02-21 22:58:14 +0800

    chore: change favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
