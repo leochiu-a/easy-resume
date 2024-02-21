116c0b8fa8273b30e49a175aeb99dd36f93c985a
leochiu <leochiu@hahow.in>
2024-02-21 19:59:08 +0800

    chore: add eslint-plugin-simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,30 @@ module.exports = {
     "next/core-web-vitals",
     "prettier",
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "simple-import-sort"],
   parser: "@typescript-eslint/parser",
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            groups: [
+              ["^react", "^@?\\w"], // External packages.
+              ["^@(/.*|$)"], // Internal packages.
+              ["^\\.\\.(?!/?$)", "^\\.\\./?$"], // Parent imports. Put `..` last.
+              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"], // // Other relative imports. Put same-folder imports and `.` last.
+              ["^\\u0000"], // Side effect imports.
+              ["^.+\\.s?css$", "^.+\\.(jpe?g|png|gif|svg)$"], // Style and image imports.
+            ],
+          },
+        ],
+      },
+    },
+  ],
 }
