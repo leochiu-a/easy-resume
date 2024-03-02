7c99154815b6c37cf1bd8da614189e21df058164
leochiu <leochiu@hahow.in>
2024-03-02 11:59:33 +0800

    refactor(typography): remove border-bottom on variant h2

--- apps/web/src/components/ui/typography.tsx
+++ apps/web/src/components/ui/typography.tsx
@@ -7,17 +7,17 @@ export const typographyVariants = cva("text-xl", {
   variants: {
     variant: {
       h1: "lg: scroll-m-20 text-5xl font-extrabold tracking-tight",
-      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+      h2: "scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0",
       h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h4: "scroll-m-20 text-xl font-semibold tracking-tight",
       p: "text-base leading-7 [&:not(:first-child)]:mt-6",
     },
     affects: {
       default: "",
-      lead: "text-xl text-muted-foreground",
+      lead: "text-muted-foreground text-xl",
       large: "text-lg font-semibold",
       small: "text-sm font-medium leading-none",
-      muted: "text-sm text-muted-foreground",
+      muted: "text-muted-foreground text-sm",
       removePMargin: "[&:not(:first-child)]:mt-0",
     },
   },
