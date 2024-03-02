ad535529f858b470aa50920b9442567536d88518
leochiu <leochiu@hahow.in>
2024-03-02 22:35:57 +0800

    feat: add template in landing page

--- apps/web/src/app/(landing-page)/page.tsx
+++ apps/web/src/app/(landing-page)/page.tsx
@@ -1,12 +1,6 @@
-import { Button } from "../../components/ui/button"
-
 /**
  * landing page 首頁
  */
 export default function Home() {
-  return (
-    <div>
-      <Button>landing page 首頁</Button>
-    </div>
-  )
+  return <div>landing page 首頁</div>
 }
