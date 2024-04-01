8c7068b4c86aa4f2ffbe7c328c42820ce30733a4
leochiu <leochiu@hahow.in>
2024-04-01 12:17:00 +0800

    refactor: use useTransition to detect if the server action is executing

--- apps/web/src/app/resumes/[id]/edit/_components/EditResumePageLayout.tsx
+++ apps/web/src/app/resumes/[id]/edit/_components/EditResumePageLayout.tsx
@@ -1,6 +1,5 @@
 "use client"
-import { KeyboardEvent, MouseEvent, useRef } from "react"
-import { useFormStatus } from "react-dom"
+import { KeyboardEvent, useTransition } from "react"
 import { FormProvider, useForm } from "react-hook-form"
 import {
   faChevronLeft,
@@ -18,14 +17,17 @@ import { Resume } from "@/types/api/resumes"
 import { ResumeForm } from "./ResumeForm"
 
 interface SaveButtonProps {
-  onClick: (e: MouseEvent<HTMLButtonElement>) => void
+  loading: boolean
 }
 
-const SaveButton = ({ onClick }: SaveButtonProps) => {
-  const { pending } = useFormStatus()
-
+const SaveButton = ({ loading }: SaveButtonProps) => {
   return (
-    <Button loading={pending} onClick={onClick} disabled={pending}>
+    <Button
+      loading={loading}
+      disabled={loading}
+      type="submit"
+      form="resume-form"
+    >
       <FontAwesomeIcon icon={faSave} className="mr-2" />
       儲存
     </Button>
@@ -37,21 +39,19 @@ interface EditResumePageLayoutProps {
 }
 
 const EditResumePageLayout = ({ resume }: EditResumePageLayoutProps) => {
-  const formRef = useRef<HTMLFormElement>(null)
+  const [isPending, startTransition] = useTransition()
 
   const formMethods = useForm<Resume>({
     defaultValues: resume,
   })
   const { trigger, getValues } = formMethods
 
-  const handleClickSaveButton = () => {
-    formRef.current?.requestSubmit()
-  }
-
   const submitAction = async () => {
     const valid = await trigger()
     if (valid) {
-      updateResume(resume.id, getValues())
+      startTransition(() => {
+        updateResume(resume.id, getValues())
+      })
     }
   }
 
@@ -60,34 +60,36 @@ const EditResumePageLayout = ({ resume }: EditResumePageLayoutProps) => {
   }
 
   return (
-    <FormProvider {...formMethods}>
-      <form ref={formRef} action={submitAction} onKeyDown={checkKeyDown}>
-        <nav className="flex bg-slate-600 px-10 py-2">
-          <Button variant="link" className="text-white" asChild>
-            <Link href="/resumes">
-              <FontAwesomeIcon icon={faChevronLeft} className="mr-2" />
-              回到我的履歷
-            </Link>
+    <>
+      <nav className="flex bg-slate-600 px-10 py-2">
+        <Button variant="link" className="text-white" asChild>
+          <Link href="/resumes">
+            <FontAwesomeIcon icon={faChevronLeft} className="mr-2" />
+            回到我的履歷
+          </Link>
+        </Button>
+
+        <div className="ml-auto flex gap-4">
+          <SaveButton loading={isPending} />
+          <Button variant="secondary">
+            <FontAwesomeIcon icon={faCrown} className="mr-2" />
+            升級使用更多模板
           </Button>
+          <Button variant="secondary">
+            <FontAwesomeIcon icon={faFilePdf} className="mr-2" />
+            下載 PDF
+          </Button>
+        </div>
+      </nav>
 
-          <div className="ml-auto flex gap-4">
-            <SaveButton onClick={handleClickSaveButton} />
-            <Button variant="secondary">
-              <FontAwesomeIcon icon={faCrown} className="mr-2" />
-              升級使用更多模板
-            </Button>
-            <Button variant="secondary">
-              <FontAwesomeIcon icon={faFilePdf} className="mr-2" />
-              下載 PDF
-            </Button>
-          </div>
-        </nav>
-
-        <main>
-          <ResumeForm />
-        </main>
-      </form>
-    </FormProvider>
+      <main>
+        <FormProvider {...formMethods}>
+          <form action={submitAction} onKeyDown={checkKeyDown} id="resume-form">
+            <ResumeForm />
+          </form>
+        </FormProvider>
+      </main>
+    </>
   )
 }
 
