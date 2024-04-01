1c9dba1a98c95bba14979ca335307e1c9d796717
leochiu <leochiu@hahow.in>
2024-04-01 21:49:42 +0800

    refactor(BasicTemplate): change styles

--- apps/web/src/components/resume-templates/BasicTemplate.tsx
+++ apps/web/src/components/resume-templates/BasicTemplate.tsx
@@ -1,18 +1,22 @@
-import { FC, Fragment } from "react"
+import { FC, forwardRef, Fragment } from "react"
 import { faCircle } from "@fortawesome/free-regular-svg-icons"
 import {
+  faEnvelope,
   faLocationDot,
-  faMobileScreen,
+  faPhone,
   faUser,
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import dayjs from "dayjs"
 
+import socialMediaIcon from "@/lib/utils/socialMediaIcon"
 import { cn } from "@/lib/utils/tailwindUtils"
 import { Field, GroupLayout, Resume } from "@/types/api/resumes"
 
 import { Avatar, AvatarImage } from "../ui/avatar"
 import { Typography } from "../ui/typography"
+
+import "./BasicTemplate.css"
 
 const { Simple } = GroupLayout
 
@@ -124,96 +128,99 @@ type BasicTemplateProps = Pick<
   | "intro"
 >
 
-const BasicTemplate: FC<BasicTemplateProps> = ({
-  wantedJob,
-  username,
-  avatarUrl,
-  city,
-  phone,
-  email,
-  socialLinks,
-  groups,
-  intro,
-}) => {
-  return (
-    <div className="bg-white px-14 py-10">
-      <div className="flex flex-col items-center gap-2">
-        <Avatar>
-          <AvatarImage src={avatarUrl} />
-        </Avatar>
-        <Typography variant="h2">{username}</Typography>
-
-        <div className="flex gap-4">
-          <span>{wantedJob}</span>
-          <span
-            className={cn(
-              "flex items-center gap-1",
-              city.length === 0 && "hidden",
-            )}
-          >
-            <FontAwesomeIcon icon={faLocationDot} className="size-5" />
-            {city}
-          </span>
-          <span
-            className={cn(
-              "flex items-center gap-1",
-              phone.length === 0 && "hidden",
-            )}
-          >
-            <FontAwesomeIcon icon={faMobileScreen} className="size-5" />
-            {phone}
-          </span>
-        </div>
-      </div>
-
-      <div className="mt-8 flex gap-10">
-        <div className="flex-1">
-          <div className="space-y-2 text-center">
-            <div className="flex items-center justify-center gap-2">
-              <FontAwesomeIcon icon={faCircle} className="size-1.5" />
-              <span className="font-semibold">詳細資訊</span>
-              <FontAwesomeIcon icon={faCircle} className="size-1.5" />
-            </div>
-            <div>
-              <div>{city}</div>
-              <div>{phone}</div>
-              <div className="underline">{email}</div>
+const BasicTemplate = forwardRef<HTMLDivElement, BasicTemplateProps>(
+  (
+    {
+      wantedJob,
+      username,
+      avatarUrl,
+      city,
+      phone,
+      email,
+      socialLinks,
+      groups,
+      intro,
+    },
+    ref,
+  ) => {
+    return (
+      <div className="bg-white p-10">
+        <div ref={ref}>
+          <div className="flex flex-col items-center gap-2">
+            <Avatar>
+              <AvatarImage src={avatarUrl} />
+            </Avatar>
+            <div className="flex items-baseline gap-4">
+              <Typography variant="h2" affects="removePMargin">
+                {username}
+              </Typography>
+              <span className="font-semibold">{wantedJob}</span>
             </div>
           </div>
 
-          {socialLinks.length > 0 && (
-            <>
-              <div className="mt-4 flex items-center justify-center gap-2">
-                <FontAwesomeIcon icon={faCircle} className="size-1.5" />
-                <span className="font-semibold">社群連結</span>
-                <FontAwesomeIcon icon={faCircle} className="size-1.5" />
-              </div>
-              <div>
-                {socialLinks.map((link, index) => (
-                  <div key={index}>
-                    <div>{link.name}</div>
-                    <div>{link.url}</div>
+          <div className="mt-8 flex gap-10 text-sm">
+            <div className="flex-1">
+              <div className="space-y-2">
+                <span className="font-semibold">個人資訊</span>
+                <div className="space-y-1">
+                  <div>
+                    <FontAwesomeIcon icon={faLocationDot} className="mr-2" />
+                    {city}
                   </div>
-                ))}
+                  <div>
+                    <FontAwesomeIcon icon={faPhone} className="mr-2" />
+                    {phone}
+                  </div>
+                  <div>
+                    <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+                    {email}
+                  </div>
+                </div>
               </div>
-            </>
-          )}
-        </div>
 
-        <div className="flex-[3] space-y-6">
-          <Section title="自我介紹" text={intro} hide={intro.length === 0} />
+              {socialLinks.length > 0 && (
+                <>
+                  <div className="mt-4 space-y-2">
+                    <span className="font-semibold">社群連結</span>
 
-          {groups.map((section, index) => (
-            <Section
-              {...section}
-              key={index}
-              hide={section.fields.length === 0}
-            />
-          ))}
+                    <div className="space-y-1">
+                      {socialLinks.map((link, index) => (
+                        <div key={index}>
+                          <FontAwesomeIcon
+                            icon={socialMediaIcon(link.name)}
+                            className="mr-2"
+                          />
+                          {link.url}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+
+            <div className="flex-[3] space-y-6">
+              <Section
+                title="自我介紹"
+                text={intro}
+                hide={intro.length === 0}
+              />
+
+              {groups.map((section, index) => (
+                <Section
+                  {...section}
+                  key={index}
+                  hide={section.fields.length === 0}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  },
+)
+
+BasicTemplate.displayName = "BasicTemplate"
 
 export default BasicTemplate
